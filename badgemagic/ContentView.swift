d7bd18e6031a0b71388975e963ce100af5e0417a
Aditya Gupta <adityaofficialgupta@gmail.com>
2019-12-21 16:33:03 +0000

    feat: added initial app skeleton

---
+++ badgemagic/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  badgemagic
+//
+//  Created by Aditya Gupta on 12/21/19.
+//  Copyright © 2019 Aditya Gupta. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
