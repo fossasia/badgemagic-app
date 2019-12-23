dc14187e24f8fedf1283566a84472938e83fe2a0
Siddharth <40488679+siddharth952@users.noreply.github.com>
2019-12-23 19:43:02 +0530

    feat: added layout and settings form
    removed Button modifiers and clipart button
    
    removed Button modifiers and clipart button

---
+++ badgemagic/Views/Extensions/Color.swift
@@ -0,0 +1,19 @@
+//
+//  Color.swift
+//  badgemagic
+//
+//  Created by Siddharth sen on 12/23/19.
+//  Copyright © 2019 Aditya Gupta. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    public static var primary_red: Color {
+        Color("primaryred", bundle: nil)
+    }
+    
+}
+
+
