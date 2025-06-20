bd55daa23ee8efdb2f0d3a105aa0f1ff14e25512
Your Name <your.email@example.com>
2025-06-20 10:16:28 +0530

    chore: commit regenerated app_localizations.dart after gen-l10n

--- lib/l10n/app_localizations.dart
+++ lib/l10n/app_localizations.dart
@@ -8,6 +8,59 @@ import 'package:intl/intl.dart' as intl;
 import 'app_localizations_en.dart';
 import 'app_localizations_zh.dart';
 
+// ignore_for_file: type=lint
+
+/// Callers can lookup localized strings with an instance of AppLocalizations
+/// returned by `AppLocalizations.of(context)`.
+///
+/// Applications need to include `AppLocalizations.delegate()` in their app's
+/// `localizationDelegates` list, and the locales they support in the app's
+/// `supportedLocales` list. For example:
+///
+/// ```dart
+/// import 'l10n/app_localizations.dart';
+///
+/// return MaterialApp(
+///   localizationsDelegates: AppLocalizations.localizationsDelegates,
+///   supportedLocales: AppLocalizations.supportedLocales,
+///   home: MyApplicationHome(),
+/// );
+/// ```
+///
+/// ## Update pubspec.yaml
+///
+/// Please make sure to update your pubspec.yaml to include the following
+/// packages:
+///
+/// ```yaml
+/// dependencies:
+///   # Internationalization support.
+///   flutter_localizations:
+///     sdk: flutter
+///   intl: any # Use the pinned version from flutter_localizations
+///
+///   # Rest of dependencies
+/// ```
+///
+/// ## iOS Applications
+///
+/// iOS applications define key application metadata, including supported
+/// locales, in an Info.plist file that is built into the application bundle.
+/// To configure the locales supported by your app, you’ll need to edit this
+/// file.
+///
+/// First, open your project’s ios/Runner.xcworkspace Xcode workspace file.
+/// Then, in the Project Navigator, open the Info.plist file under the Runner
+/// project’s Runner folder.
+///
+/// Next, select the Information Property List item, select Add Item from the
+/// Editor menu, then select Localizations from the pop-up menu.
+///
+/// Select and expand the newly-created Localizations item then, for each
+/// locale your application supports, add a new item and select the locale
+/// you wish to add from the pop-up menu in the Value field. This list should
+/// be consistent with the languages listed in the AppLocalizations.supportedLocales
+/// property.
 abstract class AppLocalizations {
   AppLocalizations(String locale)
       : localeName = intl.Intl.canonicalizedLocale(locale.toString());
@@ -20,6 +73,17 @@ abstract class AppLocalizations {
 
   static const LocalizationsDelegate<AppLocalizations> delegate =
       _AppLocalizationsDelegate();
+
+  /// A list of this localizations delegate along with the default localizations
+  /// delegates.
+  ///
+  /// Returns a list of localizations delegates containing this delegate along with
+  /// GlobalMaterialLocalizations.delegate, GlobalCupertinoLocalizations.delegate,
+  /// and GlobalWidgetsLocalizations.delegate.
+  ///
+  /// Additional delegates can be added by appending to this list in
+  /// MaterialApp. This list does not have to be used at all if a custom list
+  /// of delegates is preferred or required.
   static const List<LocalizationsDelegate<dynamic>> localizationsDelegates =
       <LocalizationsDelegate<dynamic>>[
     delegate,
@@ -28,37 +92,172 @@ abstract class AppLocalizations {
     GlobalWidgetsLocalizations.delegate,
   ];
 
+  /// A list of this localizations delegate's supported locales.
   static const List<Locale> supportedLocales = <Locale>[
     Locale('en'),
     Locale('zh')
   ];
 
+  /// No description provided for @appTitle.
+  ///
+  /// In en, this message translates to:
+  /// **'Badge Magic'**
   String get appTitle;
+
+  /// No description provided for @language.
+  ///
+  /// In en, this message translates to:
+  /// **'Language'**
   String get language;
+
+  /// No description provided for @save.
+  ///
+  /// In en, this message translates to:
+  /// **'Save'**
   String get save;
+
+  /// No description provided for @contactWithUs.
+  ///
+  /// In en, this message translates to:
+  /// **'Contact With Us'**
   String get contactWithUs;
+
+  /// No description provided for @github.
+  ///
+  /// In en, this message translates to:
+  /// **'GitHub'**
   String get github;
+
+  /// No description provided for @badgeSaved.
+  ///
+  /// In en, this message translates to:
+  /// **'Badge Saved Successfully'**
   String get badgeSaved;
+
+  /// No description provided for @selectBadge.
+  ///
+  /// In en, this message translates to:
+  /// **'Select Badge'**
   String get selectBadge;
+
+  /// No description provided for @selectLanguage.
+  ///
+  /// In en, this message translates to:
+  /// **'Select Language'**
   String get selectLanguage;
+
+  /// No description provided for @drawerCreateBadges.
+  ///
+  /// In en, this message translates to:
+  /// **'Create Badges'**
   String get drawerCreateBadges;
+
+  /// No description provided for @drawerDrawClipart.
+  ///
+  /// In en, this message translates to:
+  /// **'Draw Clipart'**
   String get drawerDrawClipart;
+
+  /// No description provided for @drawerSavedBadges.
+  ///
+  /// In en, this message translates to:
+  /// **'Saved Badges'**
   String get drawerSavedBadges;
+
+  /// No description provided for @drawerSavedCliparts.
+  ///
+  /// In en, this message translates to:
+  /// **'Saved Cliparts'**
   String get drawerSavedCliparts;
+
+  /// No description provided for @drawerSettings.
+  ///
+  /// In en, this message translates to:
+  /// **'Settings'**
   String get drawerSettings;
+
+  /// No description provided for @drawerAboutUs.
+  ///
+  /// In en, this message translates to:
+  /// **'About Us'**
   String get drawerAboutUs;
+
+  /// No description provided for @drawerOther.
+  ///
+  /// In en, this message translates to:
+  /// **'Other'**
   String get drawerOther;
+
+  /// No description provided for @drawerBuyBadge.
+  ///
+  /// In en, this message translates to:
+  /// **'Buy Badge'**
   String get drawerBuyBadge;
+
+  /// No description provided for @drawerShare.
+  ///
+  /// In en, this message translates to:
+  /// **'Share'**
   String get drawerShare;
+
+  /// No description provided for @drawerShareText.
+  ///
+  /// In en, this message translates to:
+  /// **'Badge Magic is an app to control LED name badges. This app provides features to portray names, graphics and simple animations on LED badges. You can also download it from below link https://play.google.com/store/apps/details?id=org.fossasia.badgemagic'**
   String get drawerShareText;
+
+  /// No description provided for @drawerRateUs.
+  ///
+  /// In en, this message translates to:
+  /// **'Rate Us'**
   String get drawerRateUs;
+
+  /// No description provided for @drawerFeedback.
+  ///
+  /// In en, this message translates to:
+  /// **'Feedback/Bug Reports'**
   String get drawerFeedback;
+
+  /// No description provided for @drawerPrivacyPolicy.
+  ///
+  /// In en, this message translates to:
+  /// **'Privacy Policy'**
   String get drawerPrivacyPolicy;
+
+  /// No description provided for @tabSpeed.
+  ///
+  /// In en, this message translates to:
+  /// **'Speed'**
   String get tabSpeed;
+
+  /// No description provided for @tabAnimation.
+  ///
+  /// In en, this message translates to:
+  /// **'Animation'**
   String get tabAnimation;
+
+  /// No description provided for @tabEffects.
+  ///
+  /// In en, this message translates to:
+  /// **'Effects'**
   String get tabEffects;
+
+  /// No description provided for @pleaseEnterMessage.
+  ///
+  /// In en, this message translates to:
+  /// **'Please enter a message'**
   String get pleaseEnterMessage;
+
+  /// No description provided for @badgeUpdated.
+  ///
+  /// In en, this message translates to:
+  /// **'Badge Updated Successfully'**
   String get badgeUpdated;
+
+  /// No description provided for @transfer.
+  ///
+  /// In en, this message translates to:
+  /// **'Transfer'**
   String get transfer;
 }
 
@@ -80,6 +279,7 @@ class _AppLocalizationsDelegate
 }
 
 AppLocalizations lookupAppLocalizations(Locale locale) {
+  // Lookup logic when only language code is specified.
   switch (locale.languageCode) {
     case 'en':
       return AppLocalizationsEn();
@@ -88,9 +288,8 @@ AppLocalizations lookupAppLocalizations(Locale locale) {
   }
 
   throw FlutterError(
-    'AppLocalizations.delegate failed to load unsupported locale "$locale". This is likely '
-    'an issue with the localizations generation tool. Please file an issue '
-    'on GitHub with a reproducible sample app and the gen-l10n configuration '
-    'that was used.',
-  );
+      'AppLocalizations.delegate failed to load unsupported locale "$locale". This is likely '
+      'an issue with the localizations generation tool. Please file an issue '
+      'on GitHub with a reproducible sample app and the gen-l10n configuration '
+      'that was used.');
 }
