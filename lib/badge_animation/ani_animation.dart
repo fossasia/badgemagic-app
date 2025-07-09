2d6a4d8e2b595ce1caf3a7ac8fa26004747be356
Your Name <your.email@example.com>
2025-07-09 19:35:34 +0530

    added seamless scrolling

--- lib/badge_animation/ani_animation.dart
+++ lib/badge_animation/ani_animation.dart
@@ -4,61 +4,33 @@ class AniAnimation extends BadgeAnimation {
   @override
   void processAnimation(int badgeHeight, int badgeWidth, int animationIndex,
       List<List<bool>> processGrid, List<List<bool>> canvas) {
-    int newWidth = processGrid[0].length;
-    int newHeight = processGrid.length;
-    int verticalOffset = (badgeHeight - newHeight) ~/ 2;
-    int displayWidth = newWidth > badgeWidth ? badgeWidth : newWidth;
-    int horizontalOffset = (badgeWidth - displayWidth) ~/ 2;
-    var totalAnimationLength = badgeWidth;
-    int frame = animationIndex % totalAnimationLength;
-    var firstHalf = frame < badgeWidth ~/ 2;
-    var secondHalf = frame >= badgeWidth ~/ 2;
+    int contentWidth = processGrid[0].length;
+    int contentHeight = processGrid.length;
+    int verticalOffset = (badgeHeight - contentHeight) ~/ 2;
 
+    // How many pixels to shift left for this frame
+    int scrollOffset = animationIndex % (contentWidth + badgeWidth);
+
+    // Clear the canvas
     for (int i = 0; i < badgeHeight; i++) {
       for (int j = 0; j < badgeWidth; j++) {
-        bool lineShow = false;
-        bool bitmapShowcenter = false;
-        bool bitmapShowOut = false;
+        canvas[i][j] = false;
+      }
+    }
 
-        int sourceRow = i - verticalOffset;
-        int sourceCol = j - horizontalOffset;
-
-        bool isWithinNewGrid = sourceRow >= 0 &&
-            sourceRow < newHeight &&
-            sourceCol >= 0 &&
-            sourceCol < displayWidth;
-
-        int leftCenterCol = badgeWidth ~/ 2 - 1;
-        int rightCenterCol = badgeWidth ~/ 2;
-
-        int maxDistance = leftCenterCol;
-
-        int currentAnimationIndex = animationIndex % (maxDistance + 1);
-
-        int leftColPos = leftCenterCol - currentAnimationIndex;
-        int rightColPos = rightCenterCol + currentAnimationIndex;
-
-        if (leftColPos < 0) leftColPos += badgeWidth;
-        if (rightColPos >= badgeWidth) rightColPos -= badgeWidth;
-
-        if (j == leftColPos || j == rightColPos) {
-          lineShow = true;
-        } else {
-          lineShow = false;
-        }
-
-        if (firstHalf) {
-          if (isWithinNewGrid && j > leftColPos && j < rightColPos) {
-            bitmapShowcenter = processGrid[sourceRow][sourceCol];
+    // Draw up to two copies of the content for seamless marquee effect
+    for (int copy = 0; copy < 2; copy++) {
+      int baseX = badgeWidth - scrollOffset + copy * contentWidth;
+      for (int y = 0; y < contentHeight; y++) {
+        int canvasY = y + verticalOffset;
+        if (canvasY < 0 || canvasY >= badgeHeight) continue;
+        for (int x = 0; x < contentWidth; x++) {
+          int canvasX = baseX + x;
+          if (canvasX < 0 || canvasX >= badgeWidth) continue;
+          if (processGrid[y][x]) {
+            canvas[canvasY][canvasX] = true;
           }
         }
-        if (secondHalf) {
-          if (isWithinNewGrid && (j < leftColPos || j > rightColPos)) {
-            bitmapShowOut = processGrid[sourceRow][sourceCol];
-          }
-        }
-
-        canvas[i][j] = (lineShow || bitmapShowOut || bitmapShowcenter);
       }
     }
   }
