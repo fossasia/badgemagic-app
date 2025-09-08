13eec262d71a47f599b5fb4df372001bb101242d
Your Name <your.email@example.com>
2025-09-08 15:28:50 +0530

    fixed the tile mapping logic for single animation

--- lib/view/widgets/transitiontab.dart
+++ lib/view/widgets/transitiontab.dart
@@ -98,12 +98,14 @@ class _TransitionTabState extends State<TransitionTab> {
                   icon: Icons.equalizer,
                   animationName: 'Equalizer',
                   index: 20),
+            ]),
+            _buildTileRow([
               AniContainer(
                 animation: null,
                 icon: Icons.directions_bike,
                 animationName: 'Cycle',
                 index: 21,
-              ),
+              )
             ]),
           ],
         ),
@@ -115,15 +117,22 @@ class _TransitionTabState extends State<TransitionTab> {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 4.0),
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: tiles
-            .map((tile) => Expanded(
+        mainAxisAlignment: tiles.length == 1
+            ? MainAxisAlignment.center
+            : MainAxisAlignment.spaceEvenly,
+        children: tiles.map((tile) {
+          return tiles.length == 1
+              ? Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 4.0),
+                  child: tile,
+                )
+              : Expanded(
                   child: Padding(
                     padding: const EdgeInsets.symmetric(horizontal: 4.0),
                     child: tile,
                   ),
-                ))
-            .toList(),
+                );
+        }).toList(),
       ),
     );
   }
