baf856dad54ec3dc6bf062442581e02ea7a9c70c
Sushant1806-krishna <nksushant2006@gmail.com>
2026-02-14 22:36:34 +0530

    Fix: remove unintended centering in Fixed animation (#1557)
    Removed horizontal centering offset so Fixed animation renders content flush-left without extra padding.

--- lib/badge_animation/ani_fixed.dart
+++ lib/badge_animation/ani_fixed.dart
@@ -2,17 +2,26 @@ import 'package:badgemagic/badge_animation/animation_abstract.dart';
 
 class FixedAnimation extends BadgeAnimation {
   @override
-  void processAnimation(int badgeHeight, int badgeWidth, int animationIndex,
-      List<List<bool>> processGrid, List<List<bool>> canvas) {
+  void processAnimation(
+    int badgeHeight,
+    int badgeWidth,
+    int animationIndex,
+    List<List<bool>> processGrid,
+    List<List<bool>> canvas,
+  ) {
     int newWidth = processGrid[0].length;
-    int horizontalOffset = (badgeWidth - newWidth) ~/ 2;
+
+    // Fixed animation should render content starting from the left edge
+    int horizontalOffset = 0;
 
     for (int i = 0; i < badgeHeight; i++) {
       for (int j = 0; j < badgeWidth; j++) {
         int sourceCol = j - horizontalOffset;
-        bool isWithinNewGrid = sourceCol >= 0 && sourceCol < newWidth;
-        if (isWithinNewGrid) {
+
+        if (sourceCol >= 0 && sourceCol < newWidth) {
           canvas[i][j] = processGrid[i][sourceCol];
+        } else {
+          canvas[i][j] = false;
         }
       }
     }
