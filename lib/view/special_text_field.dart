9217e39481eca954be92b9217074819ac5b7870b
MONIKA-18777 <monika.balakumar18@gmail.com>
2026-05-18 09:05:25 +0530

    fix: large integer issue for flutter web compatibility

--- lib/view/special_text_field.dart
+++ lib/view/special_text_field.dart
@@ -58,7 +58,7 @@ class ImageBuilder extends SpecialTextSpanBuilder {
     if (flag.contains(InlineImage.flag)) {
       return InlineImage(
         textStyle,
-        start: 999999999999999999,
+        start: 2147483647,
       );
     }
     return null;
