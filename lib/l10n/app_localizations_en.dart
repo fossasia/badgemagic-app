7493ba9dda269e1f41967ccc0defa459b1ed5438
MONIKA-18777 <monika.balakumar18@gmail.com>
2026-05-18 08:50:11 +0530

    Remove unrelated localization changes

---
+++ lib/l10n/app_localizations_en.dart
@@ -0,0 +1,555 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get appTitle => 'Badge Magic';
+
+  @override
+  String get checkApacheLicense => 'Check Apache License 2.0 terms used on';
+
+  @override
+  String get saveButton => 'Save';
+
+  @override
+  String get save => 'SAVE';
+
+  @override
+  String get savedBadges => 'Saved Badges';
+
+  @override
+  String get savedBadgesTitle => 'Saved Badges';
+
+  @override
+  String get drawClipart => 'Draw Clipart';
+
+  @override
+  String get drawClipartTitle => 'Draw Clipart';
+
+  @override
+  String get transferButton => 'Transfer';
+
+  @override
+  String get settings => 'Settings';
+
+  @override
+  String get language => 'Language';
+
+  @override
+  String get selectBadge => 'Select Badge';
+
+  @override
+  String get english => 'English';
+
+  @override
+  String get hindi => 'Hindi';
+
+  @override
+  String get italian => 'Italian';
+
+  @override
+  String get cancel => 'Cancel';
+
+  @override
+  String get overwrite => 'Overwrite';
+
+  @override
+  String get badgeNameExists => 'Badge name exists';
+
+  @override
+  String get similarBadgeNameExists => 'Similar badge name exists';
+
+  @override
+  String get badgeNameExistsMessage =>
+      'A badge with this name already exists. Do you want to overwrite it?';
+
+  @override
+  String get similarBadgeNameMessage =>
+      'A badge with a similar name already exists. Do you want to overwrite it?';
+
+  @override
+  String get delete => 'Delete';
+
+  @override
+  String get areYouSure => 'Are you sure?';
+
+  @override
+  String get deleteConfirmation =>
+      'This action cannot be undone. Do you want to proceed?';
+
+  @override
+  String get deleteBadgeConfirmation =>
+      'Are you sure you want to delete this badge?';
+
+  @override
+  String get error => 'Error';
+
+  @override
+  String get success => 'Success';
+
+  @override
+  String get saved => 'Saved';
+
+  @override
+  String get loading => 'Loading...';
+
+  @override
+  String get noBadgesFound => 'No badges found';
+
+  @override
+  String get noClipartFound => 'No clipart found';
+
+  @override
+  String get import => 'Import';
+
+  @override
+  String get export => 'Export';
+
+  @override
+  String get about => 'About';
+
+  @override
+  String get version => 'Version';
+
+  @override
+  String get createBadges => 'Create Badges';
+
+  @override
+  String get savedCliparts => 'Saved Cliparts';
+
+  @override
+  String get aboutUs => 'About Us';
+
+  @override
+  String get other => 'Other';
+
+  @override
+  String get shareApp => 'Share App';
+
+  @override
+  String get rateUs => 'Rate Us';
+
+  @override
+  String get privacyPolicy => 'Privacy Policy';
+
+  @override
+  String get termsOfService => 'Terms of Service';
+
+  @override
+  String get contactUs => 'Contact Us';
+
+  @override
+  String get buyBadge => 'Buy Badge';
+
+  @override
+  String get feedbackBugReports => 'Feedback/Bug Reports';
+
+  @override
+  String get shareAppText =>
+      'Badge Magic is an app to control LED name badges. This app provides features to portray names, graphics and simple animations on LED badges. You can also download it from below link https://play.google.com/store/apps/details?id=org.fossasia.badgemagic';
+
+  @override
+  String get noSavedClipart => 'No saved clipart!';
+
+  @override
+  String get noSavedClipartMessage =>
+      'Looks like there are no saved cliparts yet.';
+
+  @override
+  String get savedClipartTitle => 'Saved Clipart';
+
+  @override
+  String get aboutBadgeMagic =>
+      'Badge Magic is an app to control LED name badges. The goal is to provide options to portray names, graphics, and simple animations on LED badges. For the data transfer from the smartphone to the LED badge we use Bluetooth. The project is based on the work of Nihlcem.';
+
+  @override
+  String get developedBy => 'Developed by';
+
+  @override
+  String get fossasiaContributors => 'FOSSASIA contributors';
+
+  @override
+  String get contactWithUs => 'Contact With Us';
+
+  @override
+  String get license => 'License';
+
+  @override
+  String get speed => 'Speed';
+
+  @override
+  String get speedTitle => 'Speed';
+
+  @override
+  String get animation => 'Animation';
+
+  @override
+  String get transition => 'Transition';
+
+  @override
+  String get transitionTitle => 'Transition';
+
+  @override
+  String get effects => 'Effects';
+
+  @override
+  String get effectsTitle => 'Effects';
+
+  @override
+  String get effectsTab => 'Effects';
+
+  @override
+  String get pacman => 'Pacman';
+
+  @override
+  String get chevron => 'Chevron';
+
+  @override
+  String get diamond => 'Diamond';
+
+  @override
+  String get brokenHearts => 'Broken Hearts';
+
+  @override
+  String get cupid => 'Cupid';
+
+  @override
+  String get feet => 'Feet';
+
+  @override
+  String get fishKiss => 'Fish Kiss';
+
+  @override
+  String get diagonal => 'Diagonal';
+
+  @override
+  String get emergency => 'Emergency';
+
+  @override
+  String get beatingHearts => 'Beating Hearts';
+
+  @override
+  String get fireworks => 'Fireworks';
+
+  @override
+  String get equalizer => 'Equalizer';
+
+  @override
+  String get switchToSpecialAnimation => 'Switch to Special Animation?';
+
+  @override
+  String get specialAnimationWarning =>
+      'Selecting this animation will overwrite your current text.';
+
+  @override
+  String get copyText => 'Copy text';
+
+  @override
+  String get textCopied => 'Text copied to clipboard!';
+
+  @override
+  String get githubDescription =>
+      'Fork the repo and push changes or submit new issues.';
+
+  @override
+  String get github => 'GitHub';
+
+  @override
+  String get editingBadge => 'Editing badge';
+
+  @override
+  String get failedToLoadBadgeData => 'Failed to load badge data';
+
+  @override
+  String get saveBadge => 'Save Badge';
+
+  @override
+  String get fileName => 'File Name';
+
+  @override
+  String get createNewBadge => 'Create New Badge';
+
+  @override
+  String get enterTextHere => 'Enter text here...';
+
+  @override
+  String get applyEffects => 'Apply Effects';
+
+  @override
+  String get preview => 'Preview';
+
+  @override
+  String get sendToBadge => 'Send to Badge';
+
+  @override
+  String get savedSuccessfully => 'Saved successfully!';
+
+  @override
+  String get errorSaving => 'Error saving badge';
+
+  @override
+  String get enterBadgeName => 'Enter badge name';
+
+  @override
+  String get invertEffect => 'Invert';
+
+  @override
+  String get invertTitle => 'Invert';
+
+  @override
+  String get fixedAnimation => 'Fixed';
+
+  @override
+  String get fixedTitle => 'Fixed';
+
+  @override
+  String get flashEffect => 'Flash';
+
+  @override
+  String get marqueeEffect => 'Marquee';
+
+  @override
+  String get slow => 'Slow';
+
+  @override
+  String get normal => 'Normal';
+
+  @override
+  String get fast => 'Fast';
+
+  @override
+  String get veryFast => 'Very Fast';
+
+  @override
+  String get selectLanguage => 'Select Language';
+
+  @override
+  String get selectBadgeType => 'Select Badge Type';
+
+  @override
+  String get badgeTypeLsled => 'LS LED';
+
+  @override
+  String get badgeTypeVblab => 'VB Lab';
+
+  @override
+  String get aboutApp => 'About Badge Magic';
+
+  @override
+  String get appDescription =>
+      'Create and customize LED badge messages with ease. Design, save and share your creations.';
+
+  @override
+  String get allRightsReserved => 'All rights reserved';
+
+  @override
+  String get ok => 'OK';
+
+  @override
+  String get yes => 'Yes';
+
+  @override
+  String get no => 'No';
+
+  @override
+  String get back => 'Back';
+
+  @override
+  String get next => 'Next';
+
+  @override
+  String get done => 'Done';
+
+  @override
+  String get left => 'Left';
+
+  @override
+  String get right => 'Right';
+
+  @override
+  String get up => 'Up';
+
+  @override
+  String get down => 'Down';
+
+  @override
+  String get fixed => 'Fixed';
+
+  @override
+  String get snowflake => 'Snowflake';
+
+  @override
+  String get picture => 'Picture';
+
+  @override
+  String get laser => 'Laser';
+
+  @override
+  String get wipe => 'Wipe';
+
+  @override
+  String get inText => 'In';
+
+  @override
+  String get outText => 'Out';
+
+  @override
+  String get animationLeft => 'Left';
+
+  @override
+  String get animationRight => 'Right';
+
+  @override
+  String get animationUp => 'Up';
+
+  @override
+  String get animationDown => 'Down';
+
+  @override
+  String get animationFixed => 'Fixed';
+
+  @override
+  String get animationSnowflake => 'Snowflake';
+
+  @override
+  String get animationPicture => 'Picture';
+
+  @override
+  String get animationLaser => 'Laser';
+
+  @override
+  String get deleteSelected => 'Delete Selected';
+
+  @override
+  String get badgeImportedSuccessfully => 'Badge imported successfully';
+
+  @override
+  String get draw => 'Draw';
+
+  @override
+  String get erase => 'Erase';
+
+  @override
+  String get reset => 'Reset';
+
+  @override
+  String get clipartSavedSuccessfully => 'Clipart saved successfully';
+
+  @override
+  String get failedToSaveBadge => 'Failed to save badge';
+
+  @override
+  String get transfer => 'Transfer';
+
+  @override
+  String get pleaseEnterMessage => 'Please enter a message';
+
+  @override
+  String get badgeUpdatedSuccessfully => 'Badge updated successfully';
+
+  @override
+  String get badgeExistsMessage =>
+      'A badge with this name already exists. What would you like to do?';
+
+  @override
+  String get similarBadgeExists => 'Similar badge name exists';
+
+  @override
+  String similarBadgeExistsMessage(Object badgeName) {
+    return 'A badge with a similar name already exists: \'$badgeName\'. What would you like to do?';
+  }
+
+  @override
+  String get enterNewBadgeName => 'Please enter a new badge name.';
+
+  @override
+  String get deleteSelectedBadges => 'Delete Selected Badges';
+
+  @override
+  String get deleteBadgesConfirmation =>
+      'Are you sure you want to delete the selected badges?';
+
+  @override
+  String get transferBadge => 'Transfer Badge';
+
+  @override
+  String get transferConfirmation =>
+      'Are you sure you want to transfer the badge to the device?';
+
+  @override
+  String get editBadge => 'Edit Badge';
+
+  @override
+  String get editBadgeConfirmation => 'Do you want to edit this badge?';
+
+  @override
+  String get badgesDeletedSuccessfully =>
+      'Selected badges deleted successfully';
+
+  @override
+  String get pleaseEnterNewBadgeName => 'Please enter a new badge name.';
+
+  @override
+  String get badgeSavedSuccessfully => 'Badge saved successfully';
+
+  @override
+  String get badgeName => 'Badge Name';
+
+  @override
+  String get invertColors => 'Invert Colors';
+
+  @override
+  String get badge => 'Badge';
+
+  @override
+  String get shapes => 'Shapes';
+
+  @override
+  String get free => 'Free';
+
+  @override
+  String get square => 'Square';
+
+  @override
+  String get rectangle => 'Rectangle';
+
+  @override
+  String get circle => 'Circle';
+
+  @override
+  String get connectToBadgesWithNames =>
+      'Connect to badges with the following names';
+
+  @override
+  String get selectAll => 'Select All';
+
+  @override
+  String get clearAll => 'Clear All';
+
+  @override
+  String get addMore => 'Add More';
+
+  @override
+  String get scanSettingsSaved => 'Scan settings saved';
+
+  @override
+  String get saveSettings => 'Save Settings';
+
+  @override
+  String get connectToAnyBadge => 'Connect to any badge';
+
+  @override
+  String get badgeNameHint => 'Badge name';
+
+  @override
+  String get triangle => 'Triangle';
+
+  @override
+  String get badgeScanMode => 'Badge Scan Mode';
+}
