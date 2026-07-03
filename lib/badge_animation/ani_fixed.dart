74c9af78c81225db2f6ec17a4639a34c06645020
Riccardo Venturini <Giordanobruno227@gmail.com>
2026-07-03 13:39:41 +0200

    fixed the fixed preview

--- lib/badge_animation/ani_fixed.dart
+++ lib/badge_animation/ani_fixed.dart
@@ -4,16 +4,17 @@ class FixedAnimation extends BadgeAnimation {
   @override
   void processAnimation(int badgeHeight, int badgeWidth, int animationIndex,
       List<List<bool>> processGrid, List<List<bool>> canvas) {
-    int newWidth = processGrid[0].length;
-    int horizontalOffset = (badgeWidth - newWidth) ~/ 2;
+    int newGridHeight = processGrid.length;
+    int newGridWidth = processGrid[0].length;
+    const int startCol = 0;
 
     for (int i = 0; i < badgeHeight; i++) {
       for (int j = 0; j < badgeWidth; j++) {
-        int sourceCol = j - horizontalOffset;
-        bool isWithinNewGrid = sourceCol >= 0 && sourceCol < newWidth;
-        if (isWithinNewGrid) {
-          canvas[i][j] = processGrid[i][sourceCol];
-        }
+        bool isNewGridCell = i < newGridHeight && (startCol + j) < newGridWidth;
+        bool animationCondition =
+            (isNewGridCell && processGrid[i][startCol + j]);
+
+        canvas[i][j] = animationCondition;
       }
     }
   }
